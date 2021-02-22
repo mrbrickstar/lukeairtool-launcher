943ace49ee77cee165dfc2cd21ce53ffb2b617a9
Bendix <63739832+mrbrickstar@users.noreply.github.com>
2021-02-22 10:09:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 
 
 const {app} = require('electron');
-app.commandLine.appendSwitch ("disable-http-cache");
 const { BrowserWindow } = require('electron')
 const path = require('path');
-const express = require("express")
-const app1 = express()
-var bodyParser = require('body-parser')
-const fs = require("fs")
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const fs = require("fs");
+const { dir } = require('console');
+
 
 
 
@@ -21,6 +18,7 @@ function createWindow () {
     minWidth:800,
     minHeight: 500,
     frame: false,
+    backgroundColor: '#2a2a2a',
     icon: __dirname + './html/pics/LOGO.png',
     webPreferences: {
      preload: path.join(__dirname, 'preloader.js'),
@@ -28,29 +26,38 @@ function createWindow () {
       nodeIntegration: true,
     }
   })
-  win.loadFile('./html/index.html')
   app.commandLine.appendSwitch ("disable-http-cache");
+
+  require('dns').resolve('www.google.com', function(err) {
+    if (err) {
+      return win.loadFile('./html/nointernet.html')
+    }
+  });
+
+  win.webContents.session.clearCache(function(){
+    //some callback.
+    });
+
   try{
       JSON.parse(fs.readFileSync('./config.json'))
-  }catch(err){
-  fs.writeFileSync('./config.json','{"storein":"C:/Program Files (x86)"}')
-  }
- // win.webContents.openDevTools()
-}
+      win.loadFile('./html/index.html')
+      var store = fs.readFileSync("./config.json", "utf8");
+      store = JSON.parse(store)
+      store = store.storein
+      win.loadFile('./html/index.html')
+    }catch(err){
+    var dirname = path.normalize(path.join(__dirname, '..'));
+    dirname = path.normalize(path.join(dirname, '..'))+"/AirTool";
+    dirname = JSON.stringify(dirname)
+    fs.writeFileSync('./config.json','{"storein":'+dirname+'}')
+    win.loadFile('./html/settings.html')
 
-app1.use('/static', express.static(__dirname + '/html'));
+  }
+  // win.webContents.openDevTools()
+}
 
 
 app.whenReady().then(createWindow)
-
-app1.get("*/seatplans", async function(req, res){
-  const fs = require("fs")
-  fs.readdirSync("./seatplans/").forEach(file => {
-          console.log(file)
-      });
-  var site = fs.readFileSync("./html/seatplans1.html", "utf8")
-  res.send(site)
-})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -65,8 +72,3 @@ app.on('activate', () => {
     createWindow()
   }
 })
-
-
-app1.listen(90, function(err){
-  console.log(err)
-})  
